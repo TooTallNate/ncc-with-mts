1e8588e8d1889e0e5e38329ba801dcdfbf34cb55
Nathan Rajlich <n@n8.io>
2022-07-20 14:33:31 -0700

    Test ESM dependency usage

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,7 @@
+import slugify from '@sindresorhus/slugify';
+
+export const dogsSlug = slugify('I ♥ Dogs');
+
 export function add(a: number, b: number) {
     return a + b;
 }
--- test/util.test.ts
+++ test/util.test.ts
@@ -1,4 +1,10 @@
-import { add } from '../src/util.js';
+import { dogsSlug, add } from '../src/util.js';
+
+describe('dogsSlug', () => {
+	it('should match "i-love-dogs"', () => {
+		expect(dogsSlug).toEqual('i-love-dogs');
+	});
+});
 
 describe('add()', () => {
 	it('should add 1 + 4', () => {
